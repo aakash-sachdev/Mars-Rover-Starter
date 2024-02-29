5240af29bfc7065be3fdaa68a5f1c5ee5fc2e15b
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-02-28 21:42:05 -0600

    Task 4 | Test 8-9 | Completed | Test 9 (Under Progress)

--- rover.js
+++ rover.js
@@ -1,3 +1,6 @@
+const Command = require ("./command") 
+const Message = require ("./message")
+
 /*Class Rover details:
   Four properties:
   1) position
@@ -11,15 +14,23 @@ An object representing the mars rover.
 // Class Name Rover
 
 class Rover {
-   constructor(position, mode, generatorWatts ) {
-     this.position = position;
+   constructor(position, mode = 'NORMAL', generatorWatts = 110 ) {
+     this.position = position; // number representing the rover’s position.
      this.mode = mode ;
      this.generatorWatts = generatorWatts ;
    }
-   receiveMessage(message){
-      //handles the various types of commands it receives and updates the rover’s properties.
+   receiveMessage(message){ //handles the various types of commands it receives and updates the rover’s properties.
+      
+      let results = []; // Each element in the array is an object that corresponds to one Command in message.commands.
+
+      for (let i = 0; i < message.commands.length; i++) {
+         results.push(message.commands[i]);
+      }
+      return {
+         message: message.name,
+         result: results
+      }
    }
-
 }
-
+// console.log(Rover.receiveMessage())
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -2,12 +2,47 @@ const Rover = require('../rover.js');
 const Message = require('../message.js');
 const Command = require('../command.js');
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  // 7 test - checking that constructor sets position and default values for mode and generatorWatts:
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    let roverPosition = new Rover(55555);
+    expect(roverPosition.position).toBe(55555);
+    expect(roverPosition.mode).toBe('NORMAL');
+    expect(roverPosition.generatorWatts).toBe(110);
+  });
+  
+  // 8 test - response returned by receiveMessage contains the name of the message:
+  it("response returned by receiveMessage contains the name of the message", function() {
+    let testCommands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let message = new Message('Testing message', testCommands);
+    let rover = new Rover(66666);    // Passes 98382 as the rover's position.
+    let response = rover.receiveMessage(message);
+    expect(response.message).toBe('Testing message');
+  });
+  
+  // 9 test - response returned by receiveMessage includes two results if two commands are sent in the message:
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+    let testPosition = new Rover(77777); // New position assigned
+    let commands =  [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')]; // Sent two commands
+    let message = new Message('Test message', commands);
+    let response = testPosition.receiveMessage(message);
+    expect((response.result).length).toBe(2);
+  });
+  
+  // 10 test -responds correctly to the status check command:
+  // it("responds correctly to the status check command", function() {
+  //   let testPosition = new Rover(88888); // New position assigned
+  //   let commands =  [new Command('STATUS_CHECK')]; // checking Satus check command
+  //   let message = new Message('Test message for status check', commands);
+  //   let response = testPosition.receiveMessage(message);
+  //   expect((response.result).).toBe(2);
+  //   expect((response.result).).toBe(2);
+  //   expect((response.result).).toBe(2);
+  //   expect((response.result).).toBe(2);
+  // });
+
+
 
 });
